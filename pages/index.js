a92dab37ac492c5adf9f7c90bb5d9d4ab67a94ed
Tumo M <tumomasire@gmail.com>
2021-07-23 13:27:02 +0200

    Basic layout and data fetching from Next Api

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,53 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React, { useState} from 'react'
+import styles from 'styles/Home.module.css'
 
-export default function Home() {
+import Layout from 'components/Layout'
+import ResourceHighlight from 'components/ResourceHighlight'
+import Newsletter from 'components/Newsletter'
+import ResourceList from 'components/ResourceList'
+
+// Called at build time, and only once
+// export async function getStaticProps() {
+
+//   const resData = await fetch("http://localhost:3000/api/resources")
+//   const data = await resData.json()
+
+//   return {
+//     props: {
+//       resources: data,
+
+//     }
+//   }
+// }
+
+// Called every time the page is visited and executed on the server
+// Data is fresh
+export async function getServerSideProps() {
+
+  const resData = await fetch("http://localhost:3000/api/resources")
+  const data = await resData.json()
+
+  return {
+    props: {
+      resources: data,
+
+    }
+  }
+}
+
+const HomePage = ({resources}) => {
+
   return (
-    <h1>Hello World</h1>
+    <Layout>
+      <ResourceHighlight
+        resources={resources.slice(0,2)}
+      />
+      <Newsletter />
+      <ResourceList 
+        resources={resources.slice(2)}
+      />
+    </Layout>
   )
 }
+
+export default HomePage
